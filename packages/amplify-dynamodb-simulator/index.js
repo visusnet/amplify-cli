588eb0575c2f7c93a8be4a73165f7308ac9b0834
Alexander Rose <alexander.rose@codecentric.de>
2020-02-24 14:02:57 +0100

    fix(amplify-dynamodb-simulator): the emulator must set JAVA_OPTS first

--- packages/amplify-dynamodb-simulator/index.js
+++ packages/amplify-dynamodb-simulator/index.js
@@ -73,14 +73,16 @@ async function which(bin) {
 }
 
 function buildArgs(options) {
-  const args = ['-Djava.library.path=./DynamoDBLocal_lib', '-jar', 'DynamoDBLocal.jar', '-port', options.port];
-  if (options.dbPath) {
-    args.push('-dbPath');
-    args.push(options.dbPath);
-  }
+  const args = [];
 
   if (options.javaOpts) {
     args.push(...options.javaOpts.split(' '));
+  }
+
+  args.push(...['-Djava.library.path=./DynamoDBLocal_lib', '-jar', 'DynamoDBLocal.jar', '-port', options.port]);
+  if (options.dbPath) {
+    args.push('-dbPath');
+    args.push(options.dbPath);
   }
 
   // dbPath overrides in memory
